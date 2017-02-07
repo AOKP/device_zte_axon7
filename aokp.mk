14de4bc804ec11ab5b1caf41569bb12f1c5b4065
Tom Marshall <tdm.code@gmail.com>
2017-02-07 22:32:02 +0100

    axon7: AOKP-ify

--- aokp.mk
+++ aokp.mk
@@ -15,10 +15,10 @@
 
 $(call inherit-product, device/zte/axon7/full_axon7.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_axon7
+PRODUCT_NAME := aokp_axon7
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="P996A01_N" \
