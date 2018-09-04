f4b878621a756788ff700b0fc45bef1b73f96c2f
Tom Marshall <tdm.code@gmail.com>
2018-09-04 21:24:12 +0200

    axon7: AOKP-ify
    Change-Id: I3f1c0128a589f6dd5f83d644e34dfd8086ba40c8

--- aokp.mk
+++ aokp.mk
@@ -22,11 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ailsa_ii device
 $(call inherit-product, device/zte/axon7/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_axon7
+PRODUCT_NAME := aokp_axon7
 PRODUCT_DEVICE := axon7
 PRODUCT_BRAND := ZTE
 PRODUCT_MODEL := ZTE A2017U
